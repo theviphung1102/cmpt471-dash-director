4d541f89804ab9328d3e71c253b16fa50f732f21
lcchow <lcchow@sfu.ca>
2026-03-26 15:35:35 -0700

    addl changes to proxy selection scoring

--- selectProxy.py
+++ selectProxy.py
@@ -8,9 +8,19 @@ from datetime import datetime
 import os
 
 SERVERS = ['10.0.0.2', '10.0.0.3', '10.0.0.4']
+
+TEST_MODE = True
+TEST_SERVER = SERVERS[0]
+
 ALPHA = 0.7
-RTT_WEIGHT = 0.6
-LOAD_WEIGHT = 0.4
+
+MAX_RTT = 200.0        # ms (anything above is "bad")
+MAX_LOAD = 20.0        # users (capacity of server)
+MAX_TP = 10000.0       # kb/s (target throughput)
+
+RTT_WEIGHT = 0.7
+LOAD_WEIGHT = 0.2
+TP_WEIGHT = 0.1
 
 server_rtt = {}
 server_throughput = {}
@@ -93,32 +103,18 @@ def monitor_servers():
 
 
 def calculate_score(server):
-    # normalize values on a scale of 0 to 1. Best server gets 1, worse gets 0.
-    min_rtt = min(server_rtt.values())
-    max_rtt = max(server_rtt.values())
-    
-    min_load = min(user_count.values())
-    max_load = max(user_count.values())
+    # RTT, load, and throughput scores normalized 0 to 1
+    rtt_val = min(server_rtt[server], MAX_RTT)
+    rtt_score = 1.0 - (rtt_val / MAX_RTT)
 
-    min_tp = min(server_throughput.values())
-    max_tp = max(server_throughput.values())
+    load_val = min(user_count[server], MAX_LOAD)
+    load_score = 1.0 - (load_val / MAX_LOAD)
 
-    if max_rtt == min_rtt:
-        rtt_score = 1.0
-    else:
-        rtt_score = 1 - ((server_rtt[server] - min_rtt) / (max_rtt - min_rtt))
-    
-    if max_load == min_load:
-        load_score = 1.0
-    else:
-        load_score = 1 - ((user_count[server] - min_load) / (max_load - min_load))
+    tp_val = min(server_throughput[server], MAX_TP)
+    tp_score = tp_val / MAX_TP
 
-    if max_tp == min_tp:
-        tp_score = 1.0
-    else:
-        tp_score = (server_throughput[server] - min_tp) / (max_tp - min_tp)
-
-    score = (rtt_score + load_score + tp_score) / 3
+    # Weighted score
+    score = (RTT_WEIGHT * rtt_score) + (LOAD_WEIGHT * load_score) + (TP_WEIGHT * tp_score)
 
     server_score[server] = score
 
@@ -133,16 +129,26 @@ def select_server(client_ip):
 
     best_server = max(SERVERS, key=lambda s: server_score[s])
 
-    # new client, first request, return best server
+    # new client - currently returning first server for testing congestion on specific server link
     if not current_server:
-        client_server_assignment[client_ip] = best_server
-        logging.info(f'NEW CLIENT: Client: {client_ip} | Assigned to: {best_server} | Score: {server_score[best_server]:.6f}')
-        return best_server
+        init_server = TEST_SERVER if TEST_MODE else best_server
+        client_server_assignment[client_ip] = init_server
+
+        logging.info(f"NEW CLIENT (TestMode={TEST_MODE}): {client_ip} -> {init_server}")
+        logging.info(f"INITIAL SCORES: S1: {server_score[SERVERS[0]]:.4f} | "
+                     f"S2: {server_score[SERVERS[1]]:.4f} | S3: {server_score[SERVERS[2]]:.4f}")
         
-    # switch servers only if sufficient performance gain
-    if (server_score[best_server] - server_score[current_server]) > 0.2:
+        return init_server
+        
+    current_score = server_score[current_server]
+    best_score = server_score[best_server]
+    
+    ratio_gain = best_score / current_score if current_score > 0 else 99.9
+    abs_diff = best_score - current_score
+
+    if ratio_gain > 1.2 and abs_diff > 0.05:
         client_server_assignment[client_ip] = best_server
-        logging.info(f'SWITCH SERVER: Client: {client_ip} | {current_server} -> {best_server} | Score gain: {(server_score[best_server]/server_score[current_server]):.2f}x | Old score: {server_score[current_server]:.2f}, New score: {server_score[best_server]:.2f}, )')
+        logging.info(f'SWITCH SERVER: Client: {client_ip} | {current_server} -> {best_server} | Score gain: {ratio_gain:.2f}x | Old score: {server_score[current_server]}, New score: {server_score[best_server]}, )')
         return best_server
     
     logging.info(f'KEEP SERVER: Client: {client_ip} | Keeping: {current_server} | Score: {server_score[current_server]:.6f}')
@@ -153,13 +159,30 @@ def select_server(client_ip):
 # region ROUTING
 @app.route('/output.mpd')
 def get_mpd():
+    client_ip = request.remote_addr
+    # reset client server assignment so they get new 'best server' selection
+    if client_ip in client_server_assignment:
+        del client_server_assignment[client_ip]
+        logging.info(f"SESSION RESET: Client {client_ip} requested new manifest.")
+
     # get MPD file of any server with video
-    server = SERVERS[0]
-    response = requests.get(f'http://{server}/output.mpd')
+    content = None
+    for server in SERVERS:
+        try:
+            response = requests.get(f'http://{server}/output.mpd', timeout=2)
+            if response.status_code == 200:
+                content = response.text
+                origin_server = server
+                break
+        except requests.exceptions.RequestException:
+            logging.warning(f"GET MPD FAIL: {server} unreachable, trying next server")
+            continue
+
+    if not content:
+        return Response("No servers available for manifest", status=503)
 
     # replace MPD file URLs to point to proxy instead
-    content = response.text
-    content = content.replace(f'http://{server}', f'http://{request.host}')
+    content = content.replace(f'http://{origin_server}', f'http://{request.host}')
 
     return Response(content, mimetype='application/dash+xml')
 
@@ -188,7 +211,7 @@ def get_segment(segment):
         new_throughput = size_kb / time_taken
         server_throughput[server] = calculate_weighted_avg_throughput(server, new_throughput)
     
-    logging.info(f'GET SEGMENT: Client: {client_ip} | Server: {server} | Segment: {segment} | RTT: {new_rtt:.2f}ms | Avg RTT: {server_rtt[server]:.2f}ms | Avg Throughput: {server_throughput[server]:.2f}kb/s | Load: {user_count[server]:.2f}')
+    logging.info(f'GET SEGMENT: Client: {client_ip} | Server: {server} | Segment: {segment} | RTT: {new_rtt:.2f}ms | Avg RTT: {server_rtt[server]:.2f}ms | Avg Throughput: {server_throughput[server]:.2f}kb/s | Load: {user_count[server]}')
 
     user_count[server] -= 1
 
